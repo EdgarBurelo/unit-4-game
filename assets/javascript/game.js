30897c808ff25f2d5820f4c38f7e62737986eacc
Edgar Burelo <edgar.burelo@gmail.com>
2018-08-28 10:54:27 -0500

    Dom Modification Finished V1

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,6 +9,9 @@ var characterS = {
         charaterAttacked.healthPoints = charaterAttacked.healthPoints - this.attackPower;
         this.attackPower = this.attackPower + 10;
         //show healthpoints in dom
+        //console.log("#health"+charaterAttacked.characterName);
+        $("#health"+charaterAttacked.characterName).html(charaterAttacked.healthPoints);
+        
     },
     "dieFunction": function(player) {
         if(player.healthPoints <= 0) {
@@ -19,6 +22,7 @@ var characterS = {
         if(dff.characterStatus != "dead") {
             att.healthPoints = att.healthPoints - dff.counterAttackPower;
             //Show Healthpoints in dom
+            $("#health"+att.characterName).html(att.healthPoints);
         }
     }
 };
@@ -67,10 +71,11 @@ var gameStWrs = {
                 var name1 = element.name;
                 if(name1 != selected.characterName) {
                     gameStWrs[name1].characterType = "CPU";
-                    gameStWrs.divGenerator(name1,"characterDivAtt")
-                    $("#"+gameStWrs[name1].characterName).attr("class","col-2 imgContainer enemies");
                     //asign class="enemies"
                     //move divs enemies to "Enemies Available to attack area"
+                    gameStWrs.divGenerator(name1,"characterDivAtt")
+                    $("#"+gameStWrs[name1].characterName).attr("class","col-2 imgContainer enemies");
+                    
                 }
                 
             });
@@ -80,6 +85,7 @@ var gameStWrs = {
     },
     "characterDefend": function(defender) {
         if(!gameStWrs.defenderFalg && gameStWrs.characterSelFlag && defender.characterStatus != "dead" && defender.characterType != "Player1") {
+            $("#gameStatus").html("");
             defender.characterStatus = "defend";
             gameStWrs.defenderFalg = true;
             $("#"+defender.characterName).remove();
@@ -91,6 +97,7 @@ var gameStWrs = {
     "attackbtn": function() {
         var attacker;
         var defender;
+        $("#gameStatus").html("");
         if (gameStWrs.defenderFalg && !gameStWrs.winFlag && !gameStWrs.loseFlag) {
             $(gameStWrs.characters).each(function(index, element) {
                 attacker = element.name;
@@ -102,15 +109,17 @@ var gameStWrs = {
                             gameStWrs[defender].dieFunction(gameStWrs[defender]);
                             gameStWrs[defender].counterAttackFunction(gameStWrs[defender],gameStWrs[attacker]);
                             gameStWrs[attacker].dieFunction(gameStWrs[attacker]);
-                            
                             gameStWrs.isDead(attacker,defender);
+                            gameStWrs.winningFunc();
                         }
                     });   
                 }
             });
             
         } else {
-            console.log("there is no Character defending area");
+            //Put it in DOM
+            //console.log("there is no Character defending area");
+            $("#gameStatus").html("there is no Character defending area");
         }
 
         
@@ -119,19 +128,37 @@ var gameStWrs = {
         if(gameStWrs[att].characterStatus == "dead") {
             gameStWrs.loseFlag = true;
             console.log("You been Defeated...GAME OVER!!!");
+            $("#gameStatus").html("You been Defeated...game over!!!");
+            $("#restarBTN").attr("style","display: block;")
         }
         if(gameStWrs[dff].characterStatus == "dead") {
             gameStWrs.defenderFalg = false;
-            //desaparecer Div 
-            console.log("You have Defeated " + gameStWrs[dff].characterName + " ,you can choose to fight another enemy");
+            //desaparecer Div
+            $("#"+gameStWrs[dff].characterName).remove();
+            $("#gameStatus").html("You have Defeated " + gameStWrs[dff].characterName + " ,you can choose to fight another enemy");
+            //console.log("You have Defeated " + gameStWrs[dff].characterName + " ,you can choose to fight another enemy");
         }
 
     },
     "winningFunc": function() {
+        var counter=0;
+        $(gameStWrs.characters).each(function(index,element){
+            var name2 = element.name
+            
+            if(gameStWrs[name2].characterStatus == "dead" && gameStWrs[name2].characterType == "CPU") {
+                counter++;
+                //console.log(counter);
+                if(counter == gameStWrs.characters.length-1) {
+                    console.log("You Win");
+                    $("#gameStatus").html("You Win!!!");
+                    $("#restarBTN").attr("style","display: block;")
+                }
+            }
+        });
 
     },
     "restartGame": function() {
-
+        location.reload();
     },
     "divGenerator": function(charac1,divTar) {
         var chardiv = $("<div>");
@@ -142,6 +169,7 @@ var gameStWrs = {
         charSpanName.html(charac1);
 
         var charSpanHealth = $("<span>")
+        charSpanHealth.attr("id", "health"+charac1);
         charSpanHealth.html(gameStWrs[charac1].healthPoints);
 
         var charimg = $("<img>");
@@ -162,21 +190,25 @@ $(document).ready(function() {
     gameStWrs.characterGeneration();
     //gameStWrs.characterSelection(gameStWrs["Obi"]);
     $(document).delegate('.imgContainer','click',function(event){
-        console.log(event.currentTarget.id);
+        //console.log(event.currentTarget.id);
         var objSel = event.currentTarget.id;
         if(!gameStWrs.characterSelFlag){
-            console.log("sel1");
+            //console.log("sel1");
             gameStWrs.characterSelection(gameStWrs[objSel]);
         } else if (!gameStWrs.defenderFalg) {
-            console.log("def");
+            //console.log("def");
             gameStWrs.characterDefend(gameStWrs[objSel]);
         }
     });
     $(document).delegate('#attackBTN','click',function(event){
-        console.log(event);
+        //console.log(event);
         gameStWrs.attackbtn();
-    })
+    });
     
+    $(document).delegate('#restarBTN','click',function(event){
+        //console.log(event);
+        gameStWrs.restartGame();
+    });
     
 });
 
